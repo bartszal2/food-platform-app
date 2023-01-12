08a27c7312ef8445a7db8d854c00211a2709523c
bartszal2 <104868326+bartszal2@users.noreply.github.com>
2023-01-12 17:30:44 +0100

    Updates

---
+++ precache-manifest.8ad02890dbfff7b226b23e14aaa50aaf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "945a14b65a854c7bf1e2",
+    "url": "/food-platform-app/static/css/main.d2e4b7af.chunk.css"
+  },
+  {
+    "revision": "945a14b65a854c7bf1e2",
+    "url": "/food-platform-app/static/js/main.945a14b6.chunk.js"
+  },
+  {
+    "revision": "877ddeaf8032078f6663",
+    "url": "/food-platform-app/static/js/1.877ddeaf.chunk.js"
+  },
+  {
+    "revision": "dd2a72daac7346e9458b",
+    "url": "/food-platform-app/static/js/2.dd2a72da.chunk.js"
+  },
+  {
+    "revision": "4c3d1e7aad58feee4ec8",
+    "url": "/food-platform-app/static/js/runtime~main.4c3d1e7a.js"
+  },
+  {
+    "revision": "2792aa02febe55fc6793e8f2901fe6c2",
+    "url": "/food-platform-app/static/media/product-template.2792aa02.png"
+  },
+  {
+    "revision": "2338cebe80b3cd5b7d86393945e76c16",
+    "url": "/food-platform-app/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/food-platform-app/precache-manifest.8ad02890dbfff7b226b23e14aaa50aaf.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/food-platform-app/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
